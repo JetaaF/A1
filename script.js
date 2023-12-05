61748295808013c82cf4cb7dc208a8819c9be05e
deondaci <deondaci1@gmail.com>
2023-12-05 07:51:57 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// alert('Tryy')
+// console.log('Hello World')
+// console.log('Info e re')
+
+
+// // variabla
+// let emri = 'Jeta'
+// let mbiamri = 'Fazliu'
+// let mosha = 16
+// // tipe primiotive,, vijn me gjuhe
+// // number, string, boolean(true 1 ,fails 0),undefiemd(varibel pa vler)
+// // bigint(nr e madhe)
+// console.log(emri)
+// // nconsole e qet 'Jeta'
+
+// console.log(typeof emri)
+// //  nkonzole e qet 'Jeta' dhe tipin e variablesw,,,
+// //  ne kete rast == string
+//  nuk munesh me ja lon emrin e njejt 2 variablave
+//  me 'let' e deklarojm ni variabel
+let emri
+//  deklarimi-- krijimi  i nje variable
+emri = 'Mirejeta'
+//  inicializim -- i japim vler per her te par
+console.log(emri) //Mirejeta
+
+//  ndrrimi i vlerave
+emri = 'Jona'
+console.log(emri) //Jona
